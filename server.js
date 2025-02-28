c15a7bfb500f43611ee68ebedae92f6963745e02
Pranav <mpranavprem@gmail.com>
2025-02-28 15:45:11 +0530

    changes

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const RECAPTCHA_SECRET_KEY = "6Lf7g8YqAAAAAKmotRHMd3_hsEl6JXJdyHgwTNpM";
+const RECAPTCHA_SECRET_KEY = "6Lf9R-UqAAAAAOUfOcQGlMxW2uJMUDdfTRnXDBU_";
+// const RECAPTCHA_SECRET_KEY = "6Lf7g8YqAAAAAKmotRHMd3_hsEl6JXJdyHgwTNpM";
 const PROJECT_ID = "my-project-14519-1738150820845";
 const RECAPTCHA_SITE_KEY = "6Lf7g8YqAAAAAB1WsfCwhyYVM2vqV1BO0bp4HMdi";
 
@@ -15,15 +16,11 @@ app.post("/api/verify-recaptcha", async (req, res) => {
   const { token } = req.body;
 
   try {
-    const response = await axios.post(
-      `https://recaptchaenterprise.googleapis.com/v1/projects/${PROJECT_ID}/assessments?key=${RECAPTCHA_SECRET_KEY}`,
-      {
-        event: {
-          token,
-          siteKey: RECAPTCHA_SITE_KEY,
-        },
-      }
-    );
+    const url = new URL("https://www.google.com/recaptcha/api/siteverify");
+    url.searchParams.append("secret", RECAPTCHA_SECRET_KEY);
+    url.searchParams.append("response", token);
+
+    const res = await axios.post(url.toString());
 
     const { riskAnalysis } = response.data;
     if (riskAnalysis.score < 0.5) {
@@ -40,6 +37,32 @@ app.post("/api/verify-recaptcha", async (req, res) => {
     );
     res.status(500).json({ success: false, message: "Internal server error" });
   }
+  // try {
+  //   const response = await axios.post(
+  //     `https://recaptchaenterprise.googleapis.com/v1/projects/${PROJECT_ID}/assessments?key=${RECAPTCHA_SECRET_KEY}`,
+  //     {
+  //       event: {
+  //         token,
+  //         siteKey: RECAPTCHA_SITE_KEY,
+  //       },
+  //     }
+  //   );
+
+  //   const { riskAnalysis } = response.data;
+  //   if (riskAnalysis.score < 0.5) {
+  //     res
+  //       .status(400)
+  //       .json({ success: false, message: "reCAPTCHA verification failed" });
+  //   } else {
+  //     res.json({ success: true, score: riskAnalysis.score });
+  //   }
+  // } catch (error) {
+  //   console.error(
+  //     "Error verifying reCAPTCHA:",
+  //     error.response ? error.response.data : error.message
+  //   );
+  //   res.status(500).json({ success: false, message: "Internal server error" });
+  // }
 });
 
 const PORT = process.env.PORT || 5000;
