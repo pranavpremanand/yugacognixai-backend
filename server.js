43b68ad239835686981fa125d63dc227d1b459e4
Pranav <mpranavprem@gmail.com>
2025-02-28 16:12:28 +0530

    changes

--- server.js
+++ server.js
@@ -19,30 +19,23 @@ app.post("/api/verify-recaptcha", async (req, res) => {
     url.searchParams.append("secret", RECAPTCHA_SECRET_KEY);
     url.searchParams.append("response", token);
 
-    const res = await fetch(url, { method: "POST" });
-    // const res = await axios.post(url.toString());
-    const captchaData = await res.json();
+    const response = await fetch(url, { method: "POST" });
+    // const response = await axios.post(url.toString());
+    const captchaData = await response.json();
 
-    if(!captchaData){
-      res.status(400).json({success:false,message:"reCAPTCHA verification failed"});
+    if (!captchaData) {
+      res
+        .status(400)
+        .json({ success: false, message: "reCAPTCHA verification failed" });
     }
 
-    if (!captchaData.success||captchaData.score<0.5) {
+    if (!captchaData.success || captchaData.score < 0.5) {
       res
         .status(400)
         .json({ success: false, message: "reCAPTCHA verification failed" });
     } else {
       res.status(200).json({ success: true, score: captchaData.score });
     }
-
-    // const { riskAnalysis } = res.data;
-    // if (riskAnalysis.score < 0.5) {
-    //   res
-    //     .status(400)
-    //     .json({ success: false, message: "reCAPTCHA verification failed" });
-    // } else {
-    //   res.json({ success: true, score: riskAnalysis.score });
-    // }
   } catch (error) {
     console.error(
       "Error verifying reCAPTCHA:",
@@ -50,32 +43,6 @@ app.post("/api/verify-recaptcha", async (req, res) => {
     );
     res.status(500).json({ success: false, message: "Internal server error" });
   }
-  // try {
-  //   const response = await axios.post(
-  //     `https://recaptchaenterprise.googleapis.com/v1/projects/${PROJECT_ID}/assessments?key=${RECAPTCHA_SECRET_KEY}`,
-  //     {
-  //       event: {
-  //         token,
-  //         siteKey: RECAPTCHA_SITE_KEY,
-  //       },
-  //     }
-  //   );
-
-  //   const { riskAnalysis } = response.data;
-  //   if (riskAnalysis.score < 0.5) {
-  //     res
-  //       .status(400)
-  //       .json({ success: false, message: "reCAPTCHA verification failed" });
-  //   } else {
-  //     res.json({ success: true, score: riskAnalysis.score });
-  //   }
-  // } catch (error) {
-  //   console.error(
-  //     "Error verifying reCAPTCHA:",
-  //     error.response ? error.response.data : error.message
-  //   );
-  //   res.status(500).json({ success: false, message: "Internal server error" });
-  // }
 });
 
 const PORT = process.env.PORT || 5000;
